dcc91c8a63808ef1e300eec2c51b95e9d0a325df
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-09-27 13:41:22 +0530

    index

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
-     	<label>User Name</label>
-     	<input type="text" name="uname" placeholder="User Name"><br>
+     	<label>Username</label>
+     	<input type="text" name="uname" placeholder="Username"><br>
 
-     	<label>User Name</label>
+     	<label>Password</label>
      	<input type="password" name="password" placeholder="Password"><br>
 
      	<button type="submit">Login</button>
